dc13bd11023fc4da12d8e78c1c39ad1876fe2417
Danny Maher & Michael  Taylor <pair+D-Maher.mikejtaylor@devbootcamp.com>
2017-01-26 11:02:45 -0600

    Ship shadows working, moving on to placement

--- Battleship/app/assets/javascripts/game_setup.js
+++ Battleship/app/assets/javascripts/game_setup.js
@@ -1,8 +1,61 @@
 $(document).ready(function() {
 
-  $('.cell').on("mouseenter", function() {
-    $('.cell').css('background-color', '#CCE9F8');
-    $(this).closest('.cell').css('background-color', 'gray');
-  });
+  var boardFunctionality = function(){
+    var direction = "horizontal"
+    $("#toggle-button").on("click", function(){
+      if( direction === "horizontal"){
+        direction = "vertical";
+      }
+      else{
+        direction = "horizontal";
+      }
+    })
 
+    $('a').on("click", function(){
+      event.preventDefault();
+      $currentShip = $(this)
+      size = $(this).attr('href');
+      });
+
+      $('.cell').on("mouseenter", function() {
+        $("#warning").html("");
+        $mousePosition = $(this);
+        $currentPosition = $mousePosition
+
+        $('.ship-shadow').removeClass("ship-shadow");
+        if (direction === "horizontal"){
+          for (var i = 0 ; i <= size-1; i++){
+
+              if($currentPosition.is("td")){
+                $currentPosition.addClass("ship-shadow");
+                $currentPosition = $currentPosition.prev();
+              }
+              else{
+              $(".ship-shadow").removeClass("ship-shadow");
+                $("#warning").html("Invalid Position");
+            }}}
+          else {
+            sibIndex= $currentPosition.parent().children();
+            indexNum = sibIndex.index($currentPosition);
+            for (var i = 0 ; i <= size-1; i++){
+              if($currentPosition.is("td")){
+              $currentPosition.addClass("ship-shadow");
+              $currentPosition = $currentPosition.parent().prev();
+              // console.log($currentPosition);
+              $currentPosition = $($currentPosition.children().get(indexNum));
+            }
+            else{
+              $(".ship-shadow").removeClass("ship-shadow");
+              $("#warning").html("Invalid Position");
+
+            }}
+          }
+      });
+      $(".cell").on("click", function(){
+        $allShips= $(".ship-shadow");
+        $allShips.addClass("ship-placed");
+        $allShips.removeClass("ship-shadow")
+      });
+    };
+  boardFunctionality();
 });
